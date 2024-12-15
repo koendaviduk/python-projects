0facb17773995469b74accac3b69505abbdc87a9
davidukj <davidukj@mail.uc.edu>
2024-12-15 12:03:47 -0500

    updated comments 12/15/24

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,12 +2,16 @@
 
 import random 
 
-
+# global variable that randomizes a choice everytime the code is ran
 computer_choice = random.choice(['rock', 'paper', 'scissors'])
+
+# global variable that sets the user choice
 user_choice = input('Do you want rock, paper, or scissors?') 
 
 print('Computer choice: ', computer_choice)
 
+
+# loop function that checks the user input and determines if it matches/trumps the computer_choice value
 if computer_choice == user_choice: 
     print('TIE') 
 elif user_choice == 'rock' and computer_choice == 'scissors': 
--- rolldice.py
+++ rolldice.py
@@ -1,9 +1,14 @@
+# simple roll dice program 
+
 import random
 
+# sets a global value that rolls a random # everytime the program is run
 roll = random.randint(1, 6)
 
+# global variable gathering the user guess/input
 guess = int(input('Guess the dice roll:\n'))
 
+# main loop function checking if the user input matches the random # generated via roll variable
 if guess == roll:
     print('Correct! They rolled a ' + str(roll))
 else:
